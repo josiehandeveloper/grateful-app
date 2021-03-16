f8a5adc83844ba0e8c164f9a2aed231e2bfb79e6
Josephine Han <jsphnhan@gmail.com>
2021-03-15 17:09:29 -0700

    /api removed from endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000/api",
-  API_ENDPOINT: `https://damp-ravine-757778.herokuapp.com/api`,
+  API_ENDPOINT: `https://damp-ravine-757778.herokuapp.com`,
   TOKEN_KEY: "grateful-app-token",
 };
