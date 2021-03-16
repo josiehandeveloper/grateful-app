1b77fd28805dba0409fc189e687e8f9e5113fb16
Josephine Han <jsphnhan@gmail.com>
2021-03-15 18:50:23 -0700

    changed endpoint to new server db

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000",
-  API_ENDPOINT: "http://localhost:8000",
-  //API_ENDPOINT: `https://damp-ravine-757778.herokuapp.com`,
+  //API_ENDPOINT: "http://localhost:8000",
+  API_ENDPOINT: `https://warm-forest-63931.herokuapp.com`,
   TOKEN_KEY: "grateful-app-token",
 };
