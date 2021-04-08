26c859104491a02ced21e02648bfee0a49995b0f
Josephine Han <jsphnhan@gmail.com>
2021-04-07 21:43:48 -0700

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000",
-  API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000",
-  //API_ENDPOINT:
-  //process.env.API_ENDPOINT || `https://warm-forest-63931.herokuapp.com`,
+  //API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000",
+  API_ENDPOINT:
+    process.env.API_ENDPOINT || `https://warm-forest-63931.herokuapp.com`,
   TOKEN_KEY: "grateful-app-token",
 };
